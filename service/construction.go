4f9817da7d9828e47ee778f9dfb17cbaf0b470db
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-10-12 18:42:41 -0500

    Implement tx submit

--- service/construction.go
+++ service/construction.go
@@ -6,6 +6,10 @@ import (
 	"github.com/coinbase/rosetta-sdk-go/server"
 	"github.com/coinbase/rosetta-sdk-go/types"
 
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	ethrpl "github.com/ethereum/go-ethereum/rlp"
+
 	"github.com/figment-networks/avalanche-rosetta/client"
 )
 
@@ -23,19 +27,39 @@ func NewConstructionService(network *types.NetworkIdentifier, evmClient *client.
 	}
 }
 
+// ConstructionSubmit implements /construction/submit endpoint
+func (s ConstructionService) ConstructionSubmit(ctx context.Context, req *types.ConstructionSubmitRequest) (*types.TransactionIdentifierResponse, *types.Error) {
+	rawTx := ethcommon.Hex2Bytes(req.SignedTransaction)
+	tx := &ethtypes.Transaction{}
+
+	if err := ethrpl.DecodeBytes(rawTx, tx); err != nil {
+		return nil, errorWithInfo(errConstructionInvalidTx, err)
+	}
+
+	if err := s.evm.SendTransaction(ctx, tx); err != nil {
+		return nil, errorWithInfo(errConstructionSubmit, err)
+	}
+
+	return &types.TransactionIdentifierResponse{
+		TransactionIdentifier: &types.TransactionIdentifier{
+			Hash: tx.Hash().String(),
+		},
+	}, nil
+}
+
 // ConstructionCombine implements /construction/combine endpoint
 func (s ConstructionService) ConstructionCombine(ctx context.Context, req *types.ConstructionCombineRequest) (*types.ConstructionCombineResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
 
 // ConstructionDerive implements /construction/derive endpoint
 func (s ConstructionService) ConstructionDerive(ctx context.Context, req *types.ConstructionDeriveRequest) (*types.ConstructionDeriveResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
 
 // ConstructionHash implements /construction/hash endpoint
 func (s ConstructionService) ConstructionHash(ctx context.Context, req *types.ConstructionHashRequest) (*types.TransactionIdentifierResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
 
 // ConstructionMetadata implements /construction/metadata endpoint
@@ -45,20 +69,15 @@ func (s ConstructionService) ConstructionMetadata(ctx context.Context, req *type
 
 // ConstructionParse implements /construction/parse endpoint
 func (s ConstructionService) ConstructionParse(ctx context.Context, req *types.ConstructionParseRequest) (*types.ConstructionParseResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
 
 // ConstructionPayloads implements /construction/payloads endpoint
 func (s ConstructionService) ConstructionPayloads(ctx context.Context, req *types.ConstructionPayloadsRequest) (*types.ConstructionPayloadsResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
 
 // ConstructionPreprocess implements /construction/preprocess endpoint
 func (s ConstructionService) ConstructionPreprocess(ctx context.Context, req *types.ConstructionPreprocessRequest) (*types.ConstructionPreprocessResponse, *types.Error) {
-	return nil, errNotImplemented
-}
-
-// ConstructionSubmit implements /construction/submit endpoint
-func (s ConstructionService) ConstructionSubmit(ctx context.Context, req *types.ConstructionSubmitRequest) (*types.TransactionIdentifierResponse, *types.Error) {
-	return nil, errNotImplemented
+	return nil, errNotSupported
 }
--- service/errors.go
+++ service/errors.go
@@ -7,8 +7,9 @@ import (
 var (
 	// General errors
 	errNotImplemented = makeError(1, "Endpoint is not implemented", false)
-	errInternalError  = makeError(2, "Internal server error", true)
-	errInvalidInput   = makeError(3, "Invalid input", false)
+	errNotSupported   = makeError(2, "Endpoint is not supported", false)
+	errInternalError  = makeError(3, "Internal server error", true)
+	errInvalidInput   = makeError(4, "Invalid input", false)
 
 	// Network service errors
 	errStatusBlockFetchFailed  = makeError(100, "Unable to fetch block", true)
@@ -20,6 +21,10 @@ var (
 	errBlockInvalidInput = makeError(200, "Block number or hash is required", false)
 	errBlockFetchFailed  = makeError(201, "Unable to fetch block", true)
 	errBlockNotFound     = makeError(202, "Block was not found", false)
+
+	// Construction service errors
+	errConstructionInvalidTx = makeError(300, "Invalid transaction data", false)
+	errConstructionSubmit    = makeError(301, "Transaction submission failed", true)
 )
 
 func allErrors() []*types.Error {
@@ -42,4 +47,9 @@ func makeError(code int32, message string, retriable bool) *types.Error {
 		Message:   message,
 		Retriable: retriable,
 	}
+}
+
+func errorWithInfo(rosettaErr *types.Error, err error) *types.Error {
+	rosettaErr.Details["error"] = err.Error()
+	return rosettaErr
 }
