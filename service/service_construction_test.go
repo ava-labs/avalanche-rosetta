1209faab1cd3d27b2ef4ee0184cbcaac0349ea46
Dan Sosedoff <dan.sosedoff@gmail.com>
2020-10-28 16:13:32 -0500

    Missing file

---
+++ service/service_construction_test.go
@@ -0,0 +1,104 @@
+package service
+
+import (
+	"context"
+	"encoding/hex"
+	"testing"
+
+	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConstructionMetadata(t *testing.T) {
+	service := ConstructionService{
+		config: &Config{Mode: ModeOnline},
+	}
+
+	t.Run("unavailable in offline mode", func(t *testing.T) {
+		service := ConstructionService{
+			config: &Config{
+				Mode: ModeOffline,
+			},
+		}
+
+		resp, err := service.ConstructionMetadata(context.Background(), &types.ConstructionMetadataRequest{})
+		assert.Nil(t, resp)
+		assert.Equal(t, errUnavailableOffline.Code, err.Code)
+	})
+
+	t.Run("requires from address", func(t *testing.T) {
+		resp, err := service.ConstructionMetadata(context.Background(), &types.ConstructionMetadataRequest{})
+		assert.Nil(t, resp)
+		assert.Equal(t, errInvalidInput.Code, err.Code)
+		assert.Equal(t, "from address is not provided", err.Details["error"])
+	})
+
+	t.Run("with address", func(t *testing.T) {
+		// todo
+	})
+}
+
+func TestContructionHash(t *testing.T) {
+	service := ConstructionService{}
+
+	t.Run("no transaction", func(t *testing.T) {
+		resp, err := service.ConstructionHash(context.Background(), &types.ConstructionHashRequest{})
+		assert.Nil(t, resp)
+		assert.Equal(t, errInvalidInput.Code, err.Code)
+		assert.Equal(t, "signed transaction value is not provided", err.Details["error"])
+	})
+
+	t.Run("invalid transaction", func(t *testing.T) {
+		resp, err := service.ConstructionHash(context.Background(), &types.ConstructionHashRequest{
+			SignedTransaction: "{}",
+		})
+		assert.Nil(t, resp)
+		assert.Equal(t, errConstructionInvalidTx.Code, err.Code)
+	})
+
+	t.Run("valid transaction", func(t *testing.T) {
+		signed := `{"nonce":"0x6","gasPrice":"0x6d6e2edc00","gas":"0x5208","to":"0x85ad9d1fcf50b72255e4288dca0ad29f5f509409","value":"0xde0b6b3a7640000","input":"0x","v":"0x150f6","r":"0x64d46cc17cbdbcf73b204a6979172eb3148237ecd369181b105e92b0d7fa49a7","s":"0x285063de57245f532a14b13f605bed047a9d20ebfd0db28e01bc8cc9eaac40ee","hash":"0x92ea9280c1653aa9042c7a4d3a608c2149db45064609c18b270c7c73738e2a46"}`
+
+		resp, err := service.ConstructionHash(context.Background(), &types.ConstructionHashRequest{
+			SignedTransaction: signed,
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, "0x92ea9280c1653aa9042c7a4d3a608c2149db45064609c18b270c7c73738e2a46", resp.TransactionIdentifier.Hash)
+	})
+}
+
+func TestConstructionDerive(t *testing.T) {
+	service := ConstructionService{}
+
+	t.Run("no public key", func(t *testing.T) {
+		resp, err := service.ConstructionDerive(context.Background(), &types.ConstructionDeriveRequest{})
+		assert.Nil(t, resp)
+		assert.Equal(t, errInvalidInput.Code, err.Code)
+		assert.Equal(t, "public key is not provided", err.Details["error"])
+	})
+
+	t.Run("invalid public key", func(t *testing.T) {
+		resp, err := service.ConstructionDerive(context.Background(), &types.ConstructionDeriveRequest{
+			PublicKey: &types.PublicKey{
+				Bytes:     []byte("invaliddata"),
+				CurveType: types.Secp256k1,
+			},
+		})
+		assert.Nil(t, resp)
+		assert.Equal(t, errConstructionInvalidPubkey, err)
+	})
+
+	t.Run("valid public key", func(t *testing.T) {
+		src := "03d0156cec2e01eff9c66e5dbc3c70f98214ec90a25eb43320ebcddc1a94b677f0"
+		b, _ := hex.DecodeString(src)
+
+		resp, err := service.ConstructionDerive(context.Background(), &types.ConstructionDeriveRequest{
+			PublicKey: &types.PublicKey{
+				Bytes:     b,
+				CurveType: types.Secp256k1,
+			},
+		})
+		assert.Nil(t, err)
+		assert.Equal(t, "0x156daFC6e9A1304fD5C9AB686acB4B3c802FE3f7", resp.AccountIdentifier.Address)
+	})
+}
