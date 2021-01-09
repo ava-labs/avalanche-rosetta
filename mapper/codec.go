c936aef9a741fb7df2540f65101269d8dd2c6e81
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-01-09 11:07:08 -0600

    Update for avalanchego 1.1.1

--- mapper/codec.go
+++ mapper/codec.go
@@ -1,28 +1,33 @@
 package mapper
 
 import (
-	"github.com/ava-labs/avalanchego/utils/codec"
+	"github.com/ava-labs/avalanchego/codec"
+	"github.com/ava-labs/avalanchego/codec/linearcodec"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 	"github.com/ava-labs/coreth/plugin/evm"
 )
 
 var (
+	preApricotCodecVersion uint16 = 0
+	apricotCodecVersion    uint16 = 1
+
 	codecManager codec.Manager
 )
 
 func init() {
-	defCodec := codec.NewDefault()
-	defCodec.RegisterType(&evm.UnsignedImportTx{})
-	defCodec.RegisterType(&evm.UnsignedExportTx{})
-	defCodec.Skip(3)
-	defCodec.RegisterType(&secp256k1fx.TransferInput{})
-	defCodec.RegisterType(&secp256k1fx.MintOutput{})
-	defCodec.RegisterType(&secp256k1fx.TransferOutput{})
-	defCodec.RegisterType(&secp256k1fx.MintOperation{})
-	defCodec.RegisterType(&secp256k1fx.Credential{})
-	defCodec.RegisterType(&secp256k1fx.Input{})
-	defCodec.RegisterType(&secp256k1fx.OutputOwners{})
-
 	codecManager = codec.NewDefaultManager()
-	codecManager.RegisterCodec(uint16(0), defCodec)
+
+	preApricotCodec := linearcodec.NewDefault()
+	preApricotCodec.RegisterType(&evm.UnsignedImportTx{})
+	preApricotCodec.RegisterType(&evm.UnsignedExportTx{})
+	preApricotCodec.SkipRegistrations(3)
+	preApricotCodec.RegisterType(&secp256k1fx.TransferInput{})
+	preApricotCodec.RegisterType(&secp256k1fx.MintOutput{})
+	preApricotCodec.RegisterType(&secp256k1fx.TransferOutput{})
+	preApricotCodec.RegisterType(&secp256k1fx.MintOperation{})
+	preApricotCodec.RegisterType(&secp256k1fx.Credential{})
+	preApricotCodec.RegisterType(&secp256k1fx.Input{})
+	preApricotCodec.RegisterType(&secp256k1fx.OutputOwners{})
+
+	codecManager.RegisterCodec(preApricotCodecVersion, preApricotCodec)
 }
