0214ac4bdce95396a242ac69ffe117fce4e13f46
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-01-11 12:00:47 -0600

    Split up codec initialization; leave apricot codec init empty for now

--- mapper/codec.go
+++ mapper/codec.go
@@ -2,7 +2,9 @@ package mapper
 
 import (
 	"github.com/ava-labs/avalanchego/codec"
+	"github.com/ava-labs/avalanchego/codec/hierarchycodec"
 	"github.com/ava-labs/avalanchego/codec/linearcodec"
+	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 	"github.com/ava-labs/coreth/plugin/evm"
 )
@@ -16,18 +18,43 @@ var (
 
 func init() {
 	codecManager = codec.NewDefaultManager()
+	errs := wrappers.Errs{}
 
-	preApricotCodec := linearcodec.NewDefault()
-	preApricotCodec.RegisterType(&evm.UnsignedImportTx{})
-	preApricotCodec.RegisterType(&evm.UnsignedExportTx{})
-	preApricotCodec.SkipRegistrations(3)
-	preApricotCodec.RegisterType(&secp256k1fx.TransferInput{})
-	preApricotCodec.RegisterType(&secp256k1fx.MintOutput{})
-	preApricotCodec.RegisterType(&secp256k1fx.TransferOutput{})
-	preApricotCodec.RegisterType(&secp256k1fx.MintOperation{})
-	preApricotCodec.RegisterType(&secp256k1fx.Credential{})
-	preApricotCodec.RegisterType(&secp256k1fx.Input{})
-	preApricotCodec.RegisterType(&secp256k1fx.OutputOwners{})
+	preApricotCodec := initPreApricotCodec(&errs)
+	apricotCodec := initApricotCodec(&errs)
 
 	codecManager.RegisterCodec(preApricotCodecVersion, preApricotCodec)
+	codecManager.RegisterCodec(apricotCodecVersion, apricotCodec)
+
+	if errs.Errored() {
+		panic(errs.Err)
+	}
+}
+
+func initPreApricotCodec(errs *wrappers.Errs) linearcodec.Codec {
+	c := linearcodec.NewDefault()
+
+	errs.Add(
+		c.RegisterType(&evm.UnsignedImportTx{}),
+		c.RegisterType(&evm.UnsignedExportTx{}),
+	)
+
+	c.SkipRegistrations(3)
+
+	errs.Add(
+		c.RegisterType(&secp256k1fx.TransferInput{}),
+		c.RegisterType(&secp256k1fx.MintOutput{}),
+		c.RegisterType(&secp256k1fx.TransferOutput{}),
+		c.RegisterType(&secp256k1fx.MintOperation{}),
+		c.RegisterType(&secp256k1fx.Credential{}),
+		c.RegisterType(&secp256k1fx.Input{}),
+		c.RegisterType(&secp256k1fx.OutputOwners{}),
+	)
+
+	return c
+}
+
+// TODO: fill in type registration when C-chain implements apricot codec
+func initApricotCodec(errs *wrappers.Errs) hierarchycodec.Codec {
+	return hierarchycodec.NewDefault()
 }
