bd80c90d0a8cad2653749042944a13a917ee3328
Patrick O'Grady <prohb125@gmail.com>
2021-07-12 19:38:20 -0700

    hardcode version

--- service/rosetta.go
+++ service/rosetta.go
@@ -1,9 +1,7 @@
 package service
 
 const (
+	NodeVersion       = "1.4.10"
 	MiddlewareVersion = "0.0.2"
 	BlockchainName    = "Avalanche"
 )
-
-// NodeVersion is populated at build time
-var NodeVersion string
