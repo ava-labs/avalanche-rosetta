615ca13827a95c2c15ac9bfed098d63ef6d8aab9
Patrick O'Grady <prohb125@gmail.com>
2021-08-15 14:08:47 -0700

    fix more hex encoding

--- mapper/block.go
+++ mapper/block.go
@@ -2,19 +2,20 @@ package mapper
 
 import (
 	corethTypes "github.com/ava-labs/coreth/core/types"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 // BlockMetadata returns meta data for a block
 func BlockMetadata(block *corethTypes.Block) map[string]interface{} {
 	meta := map[string]interface{}{
-		"gas_limit":  block.GasLimit(),
-		"gas_used":   block.GasUsed(),
+		"gas_limit":  hexutil.EncodeUint64(block.GasLimit()),
+		"gas_used":   hexutil.EncodeUint64(block.GasUsed()),
 		"difficulty": block.Difficulty(),
 		"nonce":      block.Nonce(),
-		"size":       block.Size().String(),
+		"size":       hexutil.EncodeUint64(uint64(block.Size())),
 	}
 	if block.BaseFee() != nil {
-		meta["base_fee"] = block.BaseFee().String()
+		meta["base_fee"] = hexutil.EncodeBig(block.BaseFee())
 	}
 	return meta
 }
