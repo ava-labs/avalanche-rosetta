bca3fe86c8641014ba103d47340bc40cd6ed1f2d
Patrick O'Grady <prohb125@gmail.com>
2021-08-19 13:21:06 -0700

    add additional tests to confirm behavior

--- service/helper_test.go
+++ service/helper_test.go
@@ -27,4 +27,18 @@ func TestChecksumAddress(t *testing.T) {
 		assert.False(t, ok)
 		assert.Equal(t, "", addr)
 	})
+
+	t.Run("invalid length", func(t *testing.T) {
+		testAddr := "0x05da63494DfbfF6AA215E074D34aC9A25B"
+		addr, ok := ChecksumAddress(testAddr)
+		assert.False(t, ok)
+		assert.Equal(t, "", addr)
+	})
+
+	t.Run("missing 0x", func(t *testing.T) {
+		testAddr := "05da63494DfbfF6AA215E074D34aC9A25B616eF2"
+		addr, ok := ChecksumAddress(testAddr)
+		assert.True(t, ok)
+		assert.Equal(t, "0x"+testAddr, addr)
+	})
 }
