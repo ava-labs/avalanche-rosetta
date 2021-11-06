44716184fafefdfc3ff994c1b905ff4863bff9b9
conor.leary@avalabs.org <conor.leary@avalabs.org>
2021-11-05 23:10:00 -0600

    address.go

--- mapper/account.go
+++ mapper/account.go
@@ -1,15 +1,13 @@
 package mapper
 
 import (
-	"github.com/coinbase/rosetta-sdk-go/types"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 )
 
-func Account(address *ethcommon.Address) *types.AccountIdentifier {
-	if address == nil {
+func ConvertHashToAddress(hash *ethcommon.Hash) *ethcommon.Address {
+	if hash == nil {
 		return nil
 	}
-	return &types.AccountIdentifier{
-		Address: address.String(),
-	}
+	address := ethcommon.BytesToAddress(hash[13:32])
+	return &address
 }
