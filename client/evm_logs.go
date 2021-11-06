28d7f8f55604747d83b2016f1a145f844b6e98d9
conor.leary@avalabs.org <conor.leary@avalabs.org>
2021-11-05 20:18:43 -0600

    wire up evm_log_client

--- client/evm_logs.go
+++ client/evm_logs.go
@@ -2,12 +2,19 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/ava-labs/avalanche-rosetta/cache"
 	"github.com/ava-labs/coreth/core/types"
 	"github.com/ava-labs/coreth/ethclient"
 	"github.com/ava-labs/coreth/interfaces"
 	"github.com/ethereum/go-ethereum/common"
+)
+
+const (
+	defaultCacheSize   = 100
+	transferMethodHash = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 )
 
 // InfoClient is a client for the Info API
@@ -17,17 +24,37 @@ type EvmLogsClient struct {
 }
 
 // NewEthClient returns a new EVM client
+func NewEvmLogsClient(endpoint string) (*EvmLogsClient, error) {
+	endpoint = strings.TrimSuffix(endpoint, "/")
+
+	c, err := ethclient.Dial(fmt.Sprintf("%s%s", endpoint, prefixEth))
+	if err != nil {
+		return nil, err
+	}
+
+	cache := &cache.LRU{Size: 100}
+
+	return &EvmLogsClient{
+		ethClient: c,
+		cache:     cache,
+	}, nil
+}
+
+// NewEthClient returns a new EVM client
 func (c *EvmLogsClient) GetEvmLogs(ctx context.Context, blockHash *common.Hash, transactionHash *common.Hash) ([]types.Log, error) {
 	blockLogs, isCached := c.cache.Get(blockHash.String())
 
 	if !isCached {
 		var err error
-		var filter interfaces.FilterQuery = interfaces.FilterQuery{BlockHash: blockHash}
+		var topics [][]common.Hash = [][]common.Hash{{common.HexToHash(transferMethodHash)}}
+
+		var filter interfaces.FilterQuery = interfaces.FilterQuery{BlockHash: blockHash, Topics: topics}
 		blockLogs, err = c.ethClient.FilterLogs(ctx, filter)
 
 		if err != nil {
 			return nil, err
 		}
+		c.cache.Put(blockHash.String(), blockLogs)
 	}
 
 	var filteredLogs []types.Log
