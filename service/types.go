c5101be43cb5cc87f213983623cad1c98b0b158a
Conor Leary <conorleary6@gmail.com>
2021-11-15 19:36:22 -0700

    edge case cleanup

--- service/types.go
+++ service/types.go
@@ -288,3 +288,8 @@ func (m *accountMetadata) UnmarshalJSON(data []byte) error {
 	m.Nonce = nonce
 	return nil
 }
+
+// has0xPrefix validates str begins with '0x' or '0X'.
+func has0xPrefix(str string) bool {
+	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
+}
