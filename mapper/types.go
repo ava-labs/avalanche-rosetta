53fb15d3825b9a95dba4821681ccdd99bf50e403
Patrick O'Grady <prohb125@gmail.com>
2021-11-22 20:18:43 -0800

    update version

--- mapper/types.go
+++ mapper/types.go
@@ -1,17 +1,19 @@
 package mapper
 
 import (
+	"github.com/ava-labs/coreth/params"
+
 	"github.com/coinbase/rosetta-sdk-go/types"
 )
 
 const (
 	MainnetChainID       = 43114
 	MainnetAssetID       = "FvwEAhmxKfeiG8SnEvq42hc6whRyY3EFYAvebMqDNDGCgxN5Z"
-	MainnetAP5Activation = 2000000000 // TODO: SET!!
+	MainnetAP5Activation = params.AvalancheMainnetChainConfig.ApricotPhase5BlockTimestamp
 
 	FujiChainID       = 43113
 	FujiAssetID       = "U8iRqJoiJm8xZHAacmvYyZVwqQx6uDNtQeP3CQ6fcgQk3JqnK"
-	FujiAP5Activation = 1637690400 // 11/23/21 6PM UTC
+	FujiAP5Activation = params.AvalancheFujiChainConfig.ApricotPhase5BlockTimestamp
 
 	OpCall         = "CALL"
 	OpFee          = "FEE"
--- service/rosetta.go
+++ service/rosetta.go
@@ -2,6 +2,6 @@ package service
 
 const (
 	NodeVersion       = "1.7.0-fuji"
-	MiddlewareVersion = "0.0.20"
+	MiddlewareVersion = "0.0.20-fuji"
 	BlockchainName    = "Avalanche"
 )
