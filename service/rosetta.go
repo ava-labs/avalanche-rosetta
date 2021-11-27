ea6292b1a0e6865e1d4dfc8bfca06d579e682c44
Patrick O'Grady <prohb125@gmail.com>
2021-11-26 20:04:56 -0500

    AvalancheGo@v1.7.1 (#55)

--- service/rosetta.go
+++ service/rosetta.go
@@ -1,7 +1,7 @@
 package service
 
 const (
-	NodeVersion       = "1.7.0"
-	MiddlewareVersion = "0.0.20"
+	NodeVersion       = "1.7.1"
+	MiddlewareVersion = "0.0.21"
 	BlockchainName    = "Avalanche"
 )
