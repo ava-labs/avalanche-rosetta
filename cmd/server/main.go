98fe9d0fdf7459cadf7553306a6427a8f7cf0b2d
Conor Leary <conorleary6@gmail.com>
2021-12-18 21:41:41 -0700

    validate token whitelist erc20s

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -57,6 +57,10 @@ func main() {
 		log.Fatal("client init error:", err)
 	}
 
+	if err := cfg.ValidateWhitelistOnlyValidErc20s(apiClient); err != nil {
+		log.Fatal("token whitelist validation error:", err)
+	}
+
 	log.Println("starting server in", cfg.Mode, "mode")
 
 	if cfg.ChainID == 0 {
