0a2140101ac9fae606416dec68bb90fe6190864d
Patrick O'Grady <prohb125@gmail.com>
2021-12-21 09:05:57 -0600

    update versions

--- service/rosetta.go
+++ service/rosetta.go
@@ -1,7 +1,7 @@
 package service
 
 const (
-	NodeVersion       = "1.7.2"
-	MiddlewareVersion = "0.0.25"
+	NodeVersion       = "1.7.3"
+	MiddlewareVersion = "0.0.26"
 	BlockchainName    = "Avalanche"
 )
