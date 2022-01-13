775d764827541881e37e41451c6487e2ada9ecd9
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-01-13 04:17:59 -0800

    lint fix

--- mapper/peers.go
+++ mapper/peers.go
@@ -6,20 +6,28 @@ import (
 	"github.com/coinbase/rosetta-sdk-go/types"
 
 	"github.com/ava-labs/avalanchego/network"
+	"github.com/ava-labs/avalanchego/utils/wrappers"
 )
 
 func Peers(peers []network.PeerInfo) []*types.Peer {
+	var errs wrappers.Errs
+
 	result := make([]*types.Peer, len(peers))
 
 	var metadata map[string]interface{}
-	j, _ := json.Marshal(metadata)
-	json.Unmarshal(j, &metadata)
+	j, err := json.Marshal(metadata)
+	errs.Add(err)
+	errs.Add(json.Unmarshal(j, &metadata))
 
 	for idx, peer := range peers {
 		result[idx] = &types.Peer{
 			PeerID:   peer.ID,
 			Metadata: metadata,
 		}
+	}
+
+	if errs.Err != nil {
+		return []*types.Peer{}
 	}
 
 	return result
