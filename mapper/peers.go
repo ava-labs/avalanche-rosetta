0ede910e4ea979357fd98610abacf9474a18f42f
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2022-01-18 19:26:21 -0500

    cleanup info client (#65)

--- mapper/peers.go
+++ mapper/peers.go
@@ -3,16 +3,24 @@ package mapper
 import (
 	"github.com/coinbase/rosetta-sdk-go/types"
 
-	"github.com/ava-labs/avalanche-rosetta/client"
+	"github.com/ava-labs/avalanchego/network"
 )
 
-func Peers(peers []client.Peer) []*types.Peer {
+func Peers(peers []network.PeerInfo) []*types.Peer {
 	result := make([]*types.Peer, len(peers))
 
 	for idx, peer := range peers {
 		result[idx] = &types.Peer{
-			PeerID:   peer.ID,
-			Metadata: peer.Metadata(),
+			PeerID: peer.ID,
+			Metadata: map[string]interface{}{
+				"ip":              peer.IP,
+				"public_ip":       peer.PublicIP,
+				"version":         peer.Version,
+				"last_sent":       peer.LastSent,
+				"last_received":   peer.LastReceived,
+				"benched":         peer.Benched,
+				"observed_uptime": peer.ObservedUptime,
+			},
 		}
 	}
 
