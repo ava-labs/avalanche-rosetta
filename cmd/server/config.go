4f6b8455a7d36604073fc3cbdd84d4438baac5f5
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-02-01 16:18:59 -0500

    nits

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -96,11 +96,11 @@ func (c *config) Validate() error {
 func (c *config) ValidateWhitelistOnlyValidErc20s(cli client.Client) error {
 	for _, token := range c.TokenWhiteList {
 		ethAddress := ethcommon.HexToAddress(token)
-		contractInfo, err := cli.ContractInfo(ethAddress, true)
+		currency, err := cli.ContractCurrency(ethAddress, true)
 		if err != nil {
 			return err
 		}
-		if contractInfo.Decimals == client.UnknownERC20Decimals && contractInfo.Symbol == client.UnknownERC20Symbol {
+		if currency.Decimals == client.UnknownERC20Decimals && currency.Symbol == client.UnknownERC20Symbol {
 			return errInvalidErc20Address
 		}
 	}
