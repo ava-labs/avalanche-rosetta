5d40801d5abe01dd55a2e9a89c2b897a794107c9
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-02-07 13:56:23 -0500

    comments

--- client/eth.go
+++ client/eth.go
@@ -14,12 +14,14 @@ var (
 	prefixEth     = "/ext/bc/C/rpc"
 )
 
+// EthClient provides access to Coreth API
 type EthClient struct {
 	ethclient.Client
 	rpc         rpc.Requester
 	traceConfig *tracers.TraceConfig
 }
 
+// NewEthClient returns a new EVM client
 func NewEthClient(endpoint string) (*EthClient, error) {
 	endpointURL := fmt.Sprintf("%s%s", endpoint, prefixEth)
 
@@ -38,6 +40,7 @@ func NewEthClient(endpoint string) (*EthClient, error) {
 	}, nil
 }
 
+// TxPoolContent returns the tx pool content
 func (c *EthClient) TxPoolContent(ctx context.Context) (*TxPoolContent, error) {
 	content := &TxPoolContent{}
 	err := c.rpc.SendJSONRPCRequest(ctx, prefixEth, "txpool_inspect", nil, content)
@@ -47,6 +50,7 @@ func (c *EthClient) TxPoolContent(ctx context.Context) (*TxPoolContent, error) {
 	return content, err
 }
 
+// TraceTransaction returns a transaction trace
 func (c *EthClient) TraceTransaction(ctx context.Context, hash string) (*Call, []*FlatCall, error) {
 	var result *Call
 	args := []interface{}{hash, c.traceConfig}
@@ -60,6 +64,7 @@ func (c *EthClient) TraceTransaction(ctx context.Context, hash string) (*Call, [
 	return result, flattened, nil
 }
 
+// TraceBlockByHash returns the transaction traces of all transactions in the block
 func (c *EthClient) TraceBlockByHash(ctx context.Context, hash string) ([]*Call, [][]*FlatCall, error) {
 	var raw []struct {
 		*Call `json:"result"`
