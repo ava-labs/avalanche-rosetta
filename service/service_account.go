34036df4b1df6b482553321260fab7362396a5f6
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-03-16 10:22:39 -0700

    missed one

--- service/service_account.go
+++ service/service_account.go
@@ -102,7 +102,7 @@ func (s AccountService) AccountBalance(
 			return nil, wrapError(errInternalError, err)
 		}
 
-		amount := mapper.Erc20Amount(response, contractAddress, currency.Symbol, currency.Decimals, false)
+		amount := mapper.Erc20Amount(response, false, currency)
 
 		balances = append(balances, amount)
 	}
