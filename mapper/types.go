c98d0a31f6db95fbbb777926fbcf22d03b481ece
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-04-29 19:49:53 -0400

    review

--- mapper/types.go
+++ mapper/types.go
@@ -130,13 +130,11 @@ func CreateType(t string) bool {
 }
 
 func ToCurrency(symbol string, decimals uint8, contractAddress common.Address) *types.Currency {
-	mixedAddress := common.NewMixedcaseAddress(contractAddress)
-
 	return &types.Currency{
 		Symbol:   symbol,
 		Decimals: int32(decimals),
 		Metadata: map[string]interface{}{
-			ContractAddressMetadata: mixedAddress.Address().Hex(),
+			ContractAddressMetadata: contractAddress.Hex(),
 		},
 	}
 }
