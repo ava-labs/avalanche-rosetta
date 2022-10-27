d2cd584421f603808d6cfecdb6e1a0d0d6ba872e
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-27 11:55:16 -0700

    enable checks

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 make build
-ls
-./rosetta-server -config=./scripts/config.json
-# nohup make run-devnet > /dev/null 2>&1 &
+nohup ./rosetta-server -config=./scripts/config.json > /dev/null 2>&1 &
 
-# sleep 15
+sleep 15
 
-# curl -s --location --request POST 'http://localhost:8080/network/list' \
-# --header 'Content-Type: application/json' \
-# --data-raw '{
-#     "metadata" : {}
-# }'
+curl -s --location --request POST 'http://localhost:8080/network/list' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "metadata" : {}
+}'
+
 
