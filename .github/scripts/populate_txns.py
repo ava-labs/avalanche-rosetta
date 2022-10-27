7c0791195e27b8b91cc85dda24558c748d040673
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-27 14:58:08 -0700

    wow

--- .github/scripts/populate_txns.py
+++ .github/scripts/populate_txns.py
@@ -2,26 +2,24 @@ from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from eth_account import Account
 
-web3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8546"))
+web3 = Web3(Web3.HTTPProvider("http://localhost:9650/ext/bc/C/rpc"))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-print("latest block", web3.eth.block_number)
+privateKey = "0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
+acct = Account.from_key(privateKey)
+web3.eth.defaultAccount = acct
+print(web3.eth.get_balance(acct.address))
 
-dst1 = Account.create('KEYSMASH FJAFJKLDSKF7JKFDJ 2121')
-dst2 = Account.create('KEYSMASH dassaad 441')
+if len(web3.eth.accounts) == 0:
+    print(web3.geth.personal.import_raw_key(privateKey, ""))
+    web3.geth.personal.unlock_account(acct.address, '')
 
+print("latest block", web3.eth.block_number, web3.eth.accounts)
 
-for _ in range(10):
-    web3.eth.send_transaction({
-        'to': web3.eth.accounts[1],
-        'from': web3.eth.accounts[2],
-        'value': 12345
-    })
-
-    web3.eth.send_transaction({
-        'to': web3.eth.accounts[2],
-        'from': web3.eth.accounts[1],
-        'value': 54321
-    })
+web3.eth.send_transaction({
+    'to': "0x26Cb836E81bFc47c2530aDBF63968c9830a44C8d",
+    'from': acct.address,
+    'value': 12345
+})
 
 print("latest block", web3.eth.block_number)
