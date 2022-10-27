32b968fe6bd7408da82abcb06cd75ef903951906
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-27 11:50:51 -0700

    build server

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-make docker-build
-nohup make run-devnet > /dev/null 2>&1 &
+make build
+./rosetta-server -config=./script/config.json
+# nohup make run-devnet > /dev/null 2>&1 &
 
-sleep 15
+# sleep 15
 
-curl -s --location --request POST 'http://localhost:8080/network/list' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "metadata" : {}
-}'
+# curl -s --location --request POST 'http://localhost:8080/network/list' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "metadata" : {}
+# }'
 
