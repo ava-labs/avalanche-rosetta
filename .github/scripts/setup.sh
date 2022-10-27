dd06fcf8728effbb4f3b437abe6f6bbeeee81959
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-27 11:59:57 -0700

    Update setup.sh

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -5,7 +5,7 @@ nohup ./rosetta-server -config=./scripts/config.json > /dev/null 2>&1 &
 
 sleep 15
 
-curl -s --location --request POST 'http://localhost:8080/network/list' \
+curl -s --location --request POST 'http://localhost:8085/network/list' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "metadata" : {}
