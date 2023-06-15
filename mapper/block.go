e63cbddaee00cfa7732b812dce501366f143e8cc
Patrick O'Grady <prohb125@gmail.com>
2023-06-14 17:35:48 -0700

    lint

--- mapper/block.go
+++ mapper/block.go
@@ -12,7 +12,7 @@ func BlockMetadata(block *corethTypes.Block) map[string]interface{} {
 		"gas_used":   hexutil.EncodeUint64(block.GasUsed()),
 		"difficulty": block.Difficulty(),
 		"nonce":      block.Nonce(),
-		"size":       hexutil.EncodeUint64(uint64(block.Size())),
+		"size":       hexutil.EncodeUint64(block.Size()),
 	}
 	if block.BaseFee() != nil {
 		meta["base_fee"] = hexutil.EncodeBig(block.BaseFee())
